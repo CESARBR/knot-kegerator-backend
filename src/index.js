5c9f2dac4c505edd67f892c0522e8e0e3683f04b
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:27:08 -0300

    Add hapi server and `SetupTap` endpoint
    Change-Id: Iae3b75acea5d59006940042a46fbb38020daeadb

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Domain
+import SetupTap from 'interactors/SetupTap';
+import TapService from 'services/TapService';
+
+// Infrastructure
+import MongoConnection from 'infrastructure/MongoConnection';
+import CloudConnection from 'infrastructure/CloudConnection';
+import TapStore from 'infrastructure/TapStore';
+import CloudTapStore from 'infrastructure/CloudTapStore';
+import HapiAPI from 'infrastructure/HapiAPI';
+import HapiServer from 'infrastructure/HapiServer';
+
+const DB_SERVER = 'localhost:27017';
+const DB_NAME = 'test';
+
+const CLOUD_SERVER = 'knot-test.cesar.org.br';
+const CLOUD_PORT = 3000;
+const COMPANY_UUID = '76db942b-b260-49ae-a8ca-ee421fe60000';
+const COMPANY_TOKEN = '070af831b40bc587d53489d4d416a09e08261eaf';
+
+const mongoConnection = new MongoConnection(DB_SERVER, DB_NAME);
+const cloudConnetion = new CloudConnection(
+  CLOUD_SERVER,
+  CLOUD_PORT,
+  COMPANY_UUID,
+  COMPANY_TOKEN,
+);
+const tapStore = new TapStore(mongoConnection);
+const cloudTapStore = new CloudTapStore(cloudConnetion);
+
+const setupTapInteractor = new SetupTap(tapStore, cloudTapStore);
+const tapService = new TapService(setupTapInteractor);
+
+const hapiAPI = new HapiAPI(tapService);
+const hapiServer = new HapiServer(hapiAPI);
+
+async function run() {
+  await mongoConnection.start();
+  await cloudConnetion.start();
+  await hapiServer.start();
+}
+
+run();
