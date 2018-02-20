3c2b1df078542bde5c8bb8b3909cee9eb5eae00c
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:27:08 -0300

    Add hapi server and `SetupTap` endpoint
    Change-Id: Iae3b75acea5d59006940042a46fbb38020daeadb
    Reviewed-on: https://review.gerrithub.io/400705
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// Domain
+import SetupTap from 'interactors/SetupTap';
+import TapService from 'services/TapService';
+
+// Infrastructure
+import MongoConnection from 'infrastructure/MongoConnection';
+import CloudConnection from 'infrastructure/CloudConnection';
+
+import TapStore from 'infrastructure/TapStore';
+
+import DatabaseTapStore from 'infrastructure/DatabaseTapStore';
+import CloudTapStore from 'infrastructure/CloudTapStore';
+import ClientStore from 'infrastructure/ClientStore';
+import BeerStore from 'infrastructure/BeerStore';
+import KegStore from 'infrastructure/KegStore';
+
+import HapiAPI from 'infrastructure/HapiAPI';
+import HapiServer from 'infrastructure/HapiServer';
+
+const DB_SERVER = 'localhost:27017';
+const DB_NAME = 'test';
+
+const CLOUD_SERVER = 'knot-test.cesar.org.br';
+const CLOUD_PORT = 3001;
+const COMPANY_UUID = '62665279-e437-41a9-967e-e57146760000';
+const COMPANY_TOKEN = '1e6420973bbd84365f4c943302824f320b35906c';
+
+const mongoConnection = new MongoConnection(DB_SERVER, DB_NAME);
+const cloudConnetion = new CloudConnection(
+  CLOUD_SERVER,
+  CLOUD_PORT,
+  COMPANY_UUID,
+  COMPANY_TOKEN,
+);
+
+const databaseTapStore = new DatabaseTapStore(mongoConnection);
+const cloudTapStore = new CloudTapStore(cloudConnetion);
+
+const clientStore = new ClientStore(mongoConnection);
+const beerStore = new BeerStore(mongoConnection);
+const kegStore = new KegStore(mongoConnection);
+
+const tapStore = new TapStore(databaseTapStore, cloudTapStore, beerStore, kegStore);
+
+const setupTapInteractor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+const tapService = new TapService(setupTapInteractor);
+
+const hapiAPI = new HapiAPI(tapService);
+const hapiServer = new HapiServer(hapiAPI);
+
+async function run() {
+  await mongoConnection.start();
+  await cloudConnetion.start();
+  await hapiServer.start();
+}
+
+run();
---
+++ src/infrastructure/HapiServer.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-console */
+import ValidationError from 'entities/ValidationError';
+import EntityNotFoundError from 'entities/EntityNotFoundError';
+import InvalidStateError from 'entities/InvalidStateError';
+import hapi from 'hapi';
+
+const DEFAULT_HOST = 'localhost';
+const DEFAULT_PORT = 8080;
+
+function handleError(err, h) {
+  if (err instanceof ValidationError) {
+    const message = { error: err.toString(), details: err.details };
+    return h.response(message).code(422);
+  }
+  if (err instanceof EntityNotFoundError) {
+    const message = { error: err.toString(), id: err.id };
+    return h.response(message).code(404);
+  }
+  if (err instanceof InvalidStateError) {
+    const message = { error: err.toString(), id: err.id };
+    return h.response(message).code(403);
+  }
+
+  return h.response(err.toString());
+}
+
+class HapiServer {
+  constructor(kegeratorApi) {
+    this.kegeratorApi = kegeratorApi;
+  }
+
+  async start() {
+    const server = hapi.server({
+      host: process.env.HOST || DEFAULT_HOST,
+      port: parseInt(process.env.PORT, 10) || DEFAULT_PORT,
+    });
+
+    try {
+      await server.route(this.createServerRoutes());
+      await server.start();
+      console.log(`Server is running at ${server.info.uri}...`);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  createServerRoutes() {
+    const routes = [
+      {
+        method: 'POST',
+        path: '/taps/{id}/setup',
+        handler: this.setupTapHandler.bind(this),
+      },
+    ];
+
+    return routes;
+  }
+
+  async setupTapHandler(request, h) {
+    const tapData = {
+      id: request.params.id,
+      clientId: request.payload.clientId,
+      beerId: request.payload.beerId,
+      kegId: request.payload.kegId,
+    };
+
+    try {
+      await this.kegeratorApi.setupTap(tapData);
+    } catch (err) {
+      return handleError(err, h);
+    }
+
+    return h.response().code(204);
+  }
+}
+
+export default HapiServer;
