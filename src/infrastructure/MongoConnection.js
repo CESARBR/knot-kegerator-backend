0c09d0ddc08a910b22558c8b8c7397e4007b1b28
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:25:35 -0300

    Setup tap on database store
    Change-Id: Ia6174599e990ba5fec5005c14405274b47037cbb

---
+++ src/infrastructure/MongoConnection.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+class MongoConnection {
+  constructor(dbServer, dbName) {
+    this.dbURL = `mongodb://${dbServer}/${dbName}`;
+  }
+
+  async start() {
+    this.mongoose = await mongoose.connect(this.dbURL);
+    return this.mongoose;
+  }
+
+  async close() {
+    await this.mongoose.disconnect();
+  }
+
+  findOne(name, schema, query) {
+    const Model = this.mongoose.model(name, schema);
+    return Model.findOne(query);
+  }
+
+  findOneAndUpdate(name, schema, query, update) {
+    const Model = this.mongoose.model(name, schema);
+    return Model.findOneAndUpdate(query, update);
+  }
+
+  save(name, schema, data) {
+    const Model = this.mongoose.model(name, schema);
+    const dataModel = new Model(data);
+    return dataModel.save();
+  }
+
+  deleteOne(name, schema, query) {
+    const Model = this.mongoose.model(name, schema);
+    return Model.deleteOne(query);
+  }
+}
+
+export default MongoConnection;
