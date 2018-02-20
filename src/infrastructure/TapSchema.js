637d730773ca035cb0d3fba042c99c4b696729cf
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:25:35 -0300

    Setup tap on database store
    Change-Id: Ia6174599e990ba5fec5005c14405274b47037cbb

---
+++ src/infrastructure/TapSchema.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import mongooseUUID from 'mongoose-uuid2';
+
+mongooseUUID(mongoose);
+
+const TapSchema = mongoose.Schema({
+  id: {
+    type: mongoose.Types.UUID,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+  },
+  setup: {
+    type: Object,
+  },
+});
+
+export default TapSchema;
