c8a6fd70e89f578af567a93b5e7578a35ebec980
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:16:25 -0300

    Add domain rules to setup tap
    Change-Id: I6aca89bc23c2846ee49e0df7670c801c02c82f25

---
+++ test/interactors/SetupTap.spec.js
@@ -0,0 +1,171 @@
+import tape from 'tape';
+import around from 'tape-around';
+import sinon from 'sinon';
+import SetupTap from 'interactors/SetupTap';
+import Tap from 'entities/Tap';
+import Setup from 'entities/Setup';
+import Client from 'entities/Client';
+import Beer from 'entities/Beer';
+import Keg from 'entities/Keg';
+import ValidationError from 'entities/ValidationError';
+import InvalidStateError from 'entities/InvalidStateError';
+
+const client = new Client('a42e08c1-d278-444d-9451-72f70d916c61');
+
+const beer = new Beer('b60b53df-caab-41a6-9878-c00e23e504ab');
+
+const keg = new Keg('518b0ef3-c9ae-49d4-8955-7aed96022aaa');
+
+const setup = new Setup(
+  client,
+  beer,
+  keg,
+);
+
+const tapSetupMode = new Tap(
+  '9392780a-1d43-47a8-ab05-b425eeee96f4',
+  'CESAR tap',
+  true,
+  setup,
+  10.5,
+);
+
+const tapNoSetupMode = new Tap(
+  '9392780a-1d43-47a8-ab05-b425eeee96f4',
+  'CESAR tap',
+  false,
+  setup,
+  10.5,
+);
+
+function createTest(tapData, clientData, beerData, kegData) {
+  return around(tape)
+    .before((t) => {
+      const tapStore = {
+        get: sinon.stub().resolves(tapData),
+        update: sinon.stub().resolves(),
+      };
+      const clientStore = {
+        exists: sinon.stub().resolves(clientData),
+      };
+      const beerStore = {
+        exists: sinon.stub().resolves(beerData),
+      };
+      const kegStore = {
+        exists: sinon.stub().resolves(kegData),
+      };
+      const setupData = {
+        id: '9392780a-1d43-47a8-ab05-b425eeee96f4',
+        clientId: 'a42e08c1-d278-444d-9451-72f70d916c61',
+        beerId: 'b60b53df-caab-41a6-9878-c00e23e504ab',
+        kegId: '518b0ef3-c9ae-49d4-8955-7aed96022aaa',
+      };
+
+      t.next(tapStore, clientStore, beerStore, kegStore, setupData);
+    });
+}
+
+createTest(tapSetupMode, client, beer, keg)(
+  'calls updateTap() on store',
+  async (t, tapStore, clientStore, beerStore, kegStore, setupData) => {
+    const interactor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+
+    await interactor.execute(setupData);
+
+    t.true(tapStore.update.called);
+    t.end();
+  },
+);
+
+createTest(tapSetupMode, client, beer, keg)(
+  'calls updateTap() on store with the setup data',
+  async (t, tapStore, clientStore, beerStore, kegStore, setupData) => {
+    const interactor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+
+    await interactor.execute(setupData);
+    const actualTap = tapStore.update.getCall(0).args[0];
+
+    t.deepEqual(actualTap, tapSetupMode);
+    t.end();
+  },
+);
+
+createTest(tapNoSetupMode, client, beer, keg)(
+  'throws InvalidStateError if tap isn\'t in setup mode',
+  async (t, tapStore, clientStore, beerStore, kegStore, setupData) => {
+    const interactor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+
+    try {
+      await interactor.execute(setupData);
+      t.fail('should throw');
+    } catch (e) {
+      if (e instanceof InvalidStateError) {
+        t.pass('should throw');
+      } else {
+        t.fail('should throw');
+      }
+    }
+
+    t.end();
+  },
+);
+
+createTest(tapSetupMode, null, beer, keg)(
+  'throws ValidationError if client entity is missing',
+  async (t, tapStore, clientStore, beerStore, kegStore, setupData) => {
+    const interactor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+
+    try {
+      await interactor.execute(setupData);
+      t.fail('should throw');
+    } catch (e) {
+      if (e instanceof ValidationError && e.field === 'clientId') {
+        t.pass('should throw');
+      } else {
+        t.fail('should throw');
+      }
+    }
+
+    t.end();
+  },
+);
+
+createTest(tapSetupMode, client, null, keg)(
+  'throws ValidationError if beer entity is missing',
+  async (t, tapStore, clientStore, beerStore, kegStore, setupData) => {
+    const interactor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+
+    try {
+      await interactor.execute(setupData);
+      t.fail('should throw');
+    } catch (e) {
+      if (e instanceof ValidationError && e.field === 'beerId') {
+        t.pass('should throw');
+      } else {
+        t.fail('should throw');
+      }
+    }
+
+    t.end();
+  },
+);
+
+createTest(tapSetupMode, client, beer, null)(
+  'throws ValidationError if keg entity is missing',
+  async (t, tapStore, clientStore, beerStore, kegStore, setupData) => {
+    const interactor = new SetupTap(tapStore, clientStore, beerStore, kegStore);
+
+    try {
+      await interactor.execute(setupData);
+      t.fail('should throw');
+    } catch (e) {
+      if (e instanceof ValidationError && e.field === 'kegId') {
+        t.pass('should throw');
+      } else {
+        t.fail('should throw');
+      }
+    }
+
+    t.end();
+  },
+);
