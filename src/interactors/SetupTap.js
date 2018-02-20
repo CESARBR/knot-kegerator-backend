43ec932898a73a402dffd17b07d104909c6d75eb
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:16:25 -0300

    Add domain rules to setup tap
    Change-Id: I6aca89bc23c2846ee49e0df7670c801c02c82f25

---
+++ src/interactors/SetupTap.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+class SetupTap {
+  constructor(mongoStore, cloudStore) {
+    this.mongoStore = mongoStore;
+    this.cloudStore = cloudStore;
+  }
+
+  async execute(tap) {
+    if (tap.waitingSetup === 'false') {
+      const error = new Error('Tap doesn\'t in setup mode');
+      error.code = 403;
+      throw error;
+    }
+    await this.mongoStore.setupTap(tap);
+    await this.cloudStore.setupTap(tap);
+  }
+}
+
+export default SetupTap;
