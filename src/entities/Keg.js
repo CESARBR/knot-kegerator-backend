99cd8057a8634092ac834fd592c3185a9b5efa98
João Neto <joao.alexandre@cesar.org.br>
2018-02-20 14:16:25 -0300

    Add domain rules to setup tap
    Change-Id: I6aca89bc23c2846ee49e0df7670c801c02c82f25
    Reviewed-on: https://review.gerrithub.io/400713
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

---
+++ src/entities/Keg.js
@@ -0,0 +1,10 @@
+class Keg {
+  constructor(id, name, weight, totalVolume) {
+    this.id = id;
+    this.name = name;
+    this.weight = weight;
+    this.totalVolume = totalVolume;
+  }
+}
+
+export default Keg;
