fbea3fba97988a81b4c3ce2858c1d6b63abf6411
João Neto <joao.alexandre@cesar.org.br>
2018-03-19 17:03:05 -0300

    Add domain rules to list beers
    Change-Id: If5e9bb768c3b74a88ef8bedad3fba5c8a1b219c5

---
+++ test/interactors/ListBeers.spec.js
@@ -0,0 +1,60 @@
+import tape from 'tape';
+import around from 'tape-around';
+import sinon from 'sinon';
+import ListBeers from 'interactors/ListBeers';
+
+const test = around(tape)
+  .before((t) => {
+    const beersData = [
+      {
+        id: '1fb78cbb-5fc1-46fd-80e5-cf541b905324',
+        name: 'Capunga American Pale Ale',
+        brand: 'Capunga',
+        style: 'American Pale Ale',
+      },
+      {
+        id: 'ce47b845-80bf-4f1f-b939-0fe5bc271024',
+        name: 'Capunga American Lager',
+        brand: 'Capunga',
+        style: 'American Premium Lager',
+      },
+    ];
+
+    const beerStore = {
+      list: sinon.stub().resolves(beersData),
+    };
+
+    t.next(beerStore);
+  });
+
+test('calls list() on store', async (t, beerStore) => {
+  const interactor = new ListBeers(beerStore);
+
+  await interactor.execute();
+
+  t.true(beerStore.list.called);
+  t.end();
+});
+
+test('returns Beers returned by store', async (t, beerStore) => {
+  const interactor = new ListBeers(beerStore);
+  const expectedBeers = [
+    {
+      id: '1fb78cbb-5fc1-46fd-80e5-cf541b905324',
+      name: 'Capunga American Pale Ale',
+      brand: 'Capunga',
+      style: 'American Pale Ale',
+    },
+    {
+      id: 'ce47b845-80bf-4f1f-b939-0fe5bc271024',
+      name: 'Capunga American Lager',
+      brand: 'Capunga',
+      style: 'American Premium Lager',
+    },
+  ];
+
+  const actualBeers = await interactor.execute();
+
+  t.deepEqual(actualBeers, expectedBeers);
+  t.end();
+});
