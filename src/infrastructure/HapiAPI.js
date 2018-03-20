72a54b6c4f704744c464c2634cffa9a90a01f2d2
João Neto <joao.alexandre@cesar.org.br>
2018-03-20 09:47:01 -0300

    Add `ListKegs` endpoint to Hapi Server
    Change-Id: I915b887a0497af4c4f41d9e5c0144175f7430881

--- src/infrastructure/HapiAPI.js
+++ src/infrastructure/HapiAPI.js
@@ -1,9 +1,10 @@
 import { SetupTapRequest } from 'services/SetupTapRequest';
 
 class HapiAPI {
-  constructor(tapService, beerService) {
+  constructor(tapService, beerService, kegService) {
     this.tapService = tapService;
     this.beerService = beerService;
+    this.kegService = kegService;
   }
 
   async setupTap(tap) {
@@ -13,6 +14,10 @@ class HapiAPI {
 
   async listBeers() {
     return this.beerService.list();
+  }
+
+  async listKegs() {
+    return this.kegService.list();
   }
 }
 
