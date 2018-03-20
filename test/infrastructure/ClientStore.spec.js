8ec78766c407ef118d02109c79092cdc2c261566
João Neto <joao.alexandre@cesar.org.br>
2018-03-20 17:44:41 -0300

    Add infrastructure to list clients
    Change-Id: I7322d13e64c61aee91ce89fee185b55ab4db7af5

--- test/infrastructure/ClientStore.spec.js
+++ test/infrastructure/ClientStore.spec.js
@@ -3,17 +3,74 @@ import around from 'tape-around';
 import sinon from 'sinon';
 import ClientStore from 'infrastructure/ClientStore';
 
-function createTest(countData) {
+const clientsData = [
+  {
+    id: '325163c5-7f5d-46a7-beb6-45e94cb73f0f',
+    name: 'CESAR',
+  },
+  {
+    id: '3dd28356-a2bd-4a2b-ba26-22acfd2069c9',
+    name: 'Impact Hub',
+  },
+];
+
+function createTest(countData, findData) {
   return around(tape)
     .before(async (t) => {
       const mongoConnection = {
         count: sinon.stub().resolves(countData),
+        find: sinon.stub().resolves(findData),
       };
 
       t.next(mongoConnection);
     });
 }
+createTest(null, null)(
+  'ClientStore.getAll() calls find method on MongoConnection',
+  async (t, mongoConnection) => {
+    const clientStore = new ClientStore(mongoConnection);
 
+    await clientStore.getAll();
+
+    t.true(mongoConnection.find.called);
+    t.end();
+  },
+);
+
+createTest(1, clientsData)(
+  'ClientStore.getAll() returns a list of clients from MongoConnection.find()',
+  async (t, mongoConnection) => {
+    const clientStore = new ClientStore(mongoConnection);
+    const expectedClients = [
+      {
+        id: '325163c5-7f5d-46a7-beb6-45e94cb73f0f',
+        name: 'CESAR',
+      },
+      {
+        id: '3dd28356-a2bd-4a2b-ba26-22acfd2069c9',
+        name: 'Impact Hub',
+      },
+    ];
+
+    const actualClients = await clientStore.getAll();
+
+    t.deepEqual(actualClients, expectedClients);
+    t.end();
+  },
+);
+
+createTest(1, [])(
+  'ClientStore.getAll() returns a empty list if no client if found from MongoConnection.find()',
+  async (t, mongoConnection) => {
+    const clientStore = new ClientStore(mongoConnection);
+    const expectedList = [];
+
+    const actualList = await clientStore.getAll();
+
+    t.deepEqual(actualList, expectedList);
+    t.end();
+  },
+);
 createTest(null)(
   'ClientStore.exists() calls count method on MongoConnection',
   async (t, mongoConnection) => {
