684f5f30f523270f5f8b61d16a4bc505d0dbe6df
João Neto <joao.alexandre@cesar.org.br>
2018-03-20 17:44:41 -0300

    Add infrastructure to list clients
    Change-Id: I7322d13e64c61aee91ce89fee185b55ab4db7af5

--- src/infrastructure/ClientSchema.js
+++ src/infrastructure/ClientSchema.js
@@ -14,4 +14,6 @@ const ClientSchema = mongoose.Schema({
   },
 });
 
+ClientSchema.set('toObject', { getters: true });
+
 export default ClientSchema;
--- src/infrastructure/ClientStore.js
+++ src/infrastructure/ClientStore.js
@@ -5,6 +5,11 @@ class ClientStore {
     this.connection = connection;
   }
 
+  async getAll() {
+    const result = await this.connection.find('Client', ClientSchema);
+    return result;
+  }
+
   async exists(id) {
     const result = await this.connection.count('Client', ClientSchema, { id });
     if (!result) {
