dcce26367827beea6107c2204e50b7648e96a384
João Neto <joao.alexandre@cesar.org.br>
2018-03-20 17:37:22 -0300

    Add domain rules to list clients
    Change-Id: I801f73e6207e6296dc4b291e4d08a6023aa82cd1

---
+++ test/interactors/ListClients.spec.js
@@ -0,0 +1,51 @@
+import tape from 'tape';
+import around from 'tape-around';
+import sinon from 'sinon';
+import ListClients from 'interactors/ListClients';
+
+const test = around(tape)
+  .before((t) => {
+    const clientsData = [
+      {
+        id: '325163c5-7f5d-46a7-beb6-45e94cb73f0f',
+        name: 'CESAR',
+      },
+      {
+        id: '3dd28356-a2bd-4a2b-ba26-22acfd2069c9',
+        name: 'Impact Hub',
+      },
+    ];
+    const clientStore = {
+      getAll: sinon.stub().resolves(clientsData),
+    };
+
+    t.next(clientStore);
+  });
+
+test('calls getAll() on store', async (t, clientStore) => {
+  const interactor = new ListClients(clientStore);
+
+  await interactor.execute();
+
+  t.true(clientStore.getAll.called);
+  t.end();
+});
+
+test('returns Clients returned by store', async (t, clientStore) => {
+  const interactor = new ListClients(clientStore);
+  const expectedClients = [
+    {
+      id: '325163c5-7f5d-46a7-beb6-45e94cb73f0f',
+      name: 'CESAR',
+    },
+    {
+      id: '3dd28356-a2bd-4a2b-ba26-22acfd2069c9',
+      name: 'Impact Hub',
+    },
+  ];
+
+  const actualClients = await interactor.execute();
+
+  t.deepEqual(actualClients, expectedClients);
+  t.end();
+});
