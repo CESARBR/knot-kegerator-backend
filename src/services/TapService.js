1e4dee6934cccc001a83922ab793bd36f353f655
João Neto <joao.alexandre@cesar.org.br>
2018-03-21 15:18:40 -0300

    Add domain rules to list taps
    Change-Id: I9092a9caf516578dbf5ce469a7de260025031489
    Reviewed-on: https://review.gerrithub.io/404988
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- src/services/TapService.js
+++ src/services/TapService.js
@@ -1,13 +1,18 @@
 import { SetupTapRequestValidator } from './SetupTapRequest';
 
 class TapService {
-  constructor(setupTapInteractor) {
+  constructor(setupTapInteractor, listTapsInteractor) {
     this.setupTapInteractor = setupTapInteractor;
+    this.listTapsInteractor = listTapsInteractor;
   }
 
   async setup(request) {
     SetupTapRequestValidator.validate(request);
     await this.setupTapInteractor.execute(request);
+  }
+
+  async list() {
+    return this.listTapsInteractor.execute();
   }
 }
 
--- test/interactors/ListTaps.spec.js
+++ test/interactors/ListTaps.spec.js
@@ -1,8 +1,7 @@
 import tape from 'tape';
 import around from 'tape-around';
 import sinon from 'sinon';
-import TapService from 'services/TapService';
-import { SetupTapRequest } from 'services/SetupTapRequest';
+import ListTaps from 'interactors/ListTaps';
 
 const test = around(tape)
   .before((t) => {
@@ -37,25 +36,25 @@ const test = around(tape)
         waitingSetup: true,
       },
     ];
-    const setupTapInteractor = {
-      execute: sinon.stub().resolves(),
-    };
-    const listTapsInteractor = {
-      execute: sinon.stub().resolves(tapsData),
+
+    const tapStore = {
+      list: sinon.stub().resolves(tapsData),
     };
 
-    const tapService = new TapService(setupTapInteractor, listTapsInteractor);
-    t.next(tapService);
+    t.next(tapStore);
   });
 
-test('list() calls ListTaps.execute()', async (t, tapService) => {
-  await tapService.list();
+test('calls list() on store', async (t, tapStore) => {
+  const interactor = new ListTaps(tapStore);
 
-  t.true(tapService.listTapsInteractor.execute.called);
+  await interactor.execute();
+
+  t.true(tapStore.list.called);
   t.end();
 });
 
-test('list() returns Taps returned by ListTaps.execute()', async (t, tapService) => {
+test('returns Taps returned by store', async (t, tapStore) => {
+  const interactor = new ListTaps(tapStore);
   const expectedTaps = [
     {
       id: '7bfe7203-2617-4590-bfac-8d48923fbf01',
@@ -87,45 +86,9 @@ test('list() returns Taps returned by ListTaps.execute()', async (t, tapService)
       waitingSetup: true,
     },
   ];
-  const actualTaps = await tapService.list();
+
+  const actualTaps = await interactor.execute();
 
   t.deepEqual(actualTaps, expectedTaps);
-  t.end();
-});
-
-test('setup() calls SetupTap.execute()', async (t, tapService) => {
-  const request = new SetupTapRequest(
-    '9392780a-1d43-47a8-ab05-b425eeee96f4',
-    'a42e08c1-d278-444d-9451-72f70d916c61',
-    'b60b53df-caab-41a6-9878-c00e23e504ab',
-    '518b0ef3-c9ae-49d4-8955-7aed96022aaa',
-  );
-  await tapService.setup(request);
-
-  t.true(tapService.setupTapInteractor.execute.called);
-  t.end();
-});
-
-test('setup() calls SetupTap.execute() with request', async (t, tapService) => {
-  const request = new SetupTapRequest(
-    '9392780a-1d43-47a8-ab05-b425eeee96f4',
-    'a42e08c1-d278-444d-9451-72f70d916c61',
-    'b60b53df-caab-41a6-9878-c00e23e504ab',
-    '518b0ef3-c9ae-49d4-8955-7aed96022aaa',
-  );
-  await tapService.setup(request);
-
-  const actualRequest = tapService.setupTapInteractor.execute.getCall(0).args[0];
-  t.deepEqual(actualRequest, request);
-  t.end();
-});
-
-test('setup() validates request', async (t, tapService) => {
-  try {
-    await tapService.setup();
-    t.fail('should throw');
-  } catch (e) {
-    t.pass('should throw');
-  }
   t.end();
 });
